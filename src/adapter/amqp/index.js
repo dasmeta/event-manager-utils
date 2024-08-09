2aaddf7560a8199d3a296fbd25b80338c3be0e50
hovhannes.babayan <hovhannes.babayan@fouraitch.com>
2024-08-09 11:50:21 +0400

    change amqp connection to use pooling

--- src/adapter/amqp/index.js
+++ src/adapter/amqp/index.js
@@ -1,5 +1,6 @@
 const amqplib = require('amqplib');
-const  { Buffer } = require('buffer');
+const { Buffer } = require('buffer');
+const genericPool = require('generic-pool');
 
 class messageAdapter {
     #AMQPMessage;
@@ -44,7 +45,7 @@ class subscriptionAdapter {
     async exists(...props) {
         await this.#topic.exists();
 
-        this.#channel = await (this.#topic.channel)
+        this.#channel = await (this.#topic.getChannel())
         await this.#channel.assertQueue(this.#name, {
             exclusive: true
         })
@@ -82,9 +83,9 @@ class subscriptionAdapter {
     }
 
     removeListener(key, callback) {
-        this.#topic.channel.then(ch => {
-            ch.cancel(this.#name)
-        });
+        if(this.#channel) {
+            this.#channel.cancel(this.#name)
+        }
     }
 }
 
@@ -92,14 +93,19 @@ class topicAdapter {
     /**
      * @type string
      */
+    #getChannel;
     #exchangeType;
+    #pool;
 
-    constructor(name, channel) {
+    constructor(name, getChannel, pool) {
         /* Rabbitmq exchanger name */
         this.name = name;
 
-        /* Rabbitmq opened channel */
-        this.channel = channel;
+        /* Function to get a RabbitMQ channel */
+        this.#getChannel = getChannel;
+
+        /* Connection pool */
+        this.#pool = pool;
 
         /* Rabbitmq exchanger type */
         this.#exchangeType = process.env.MQ_EXCHANGE_TYPE || 'fanout';
@@ -115,23 +121,33 @@ class topicAdapter {
     }
 
     async create(...props) {
-        const channel = await this.channel;
-        channel.assertQueue(this.name, {
-            durable: true
-        });
-        channel.assertExchange(this.name, this.#exchangeType, {
-            durable: true,
-            ...props
-        });
-        return channel.bindQueue(this.name, this.name);
+        const channel = await this.#getChannel();
+        try {
+            channel.assertQueue(this.name, {
+                durable: true
+            });
+            channel.assertExchange(this.name, this.#exchangeType, {
+                durable: true,
+                ...props
+            });
+            return channel.bindQueue(this.name, this.name);
+        } finally {
+            this.#pool.release(channel);
+        }
+       
     }
 
     async publish(...props) {
-        return (await this.channel).publish(
-            this.name,
-            '',
-            ...props
-        );
+        const channel = await this.#getChannel();
+        try {
+            return channel.publish(
+                this.name,
+                '',
+                ...props
+            );
+        } finally {
+            await this.#pool.release(channel);
+        }
     }
 }
 
@@ -140,24 +156,80 @@ class topicAdapter {
  *
  * Required env
  * - RABBITMQ_URL || AMQP_URL
+ * 
+ * Not required env
+ * 
+ * - MQ_CLIENT_RECONNECT_TIMEOUT
+ * - MQ_CLIENT_MAX_POOL_SIZE
+ * - MQ_CLIENT_MIN_POOL_SIZE
  */
 class clientAdapter {
     /**
      * @type function
      */
     #getChannel;
+    #connection;
+    #reconnectTimeout;
+    #channelPool;
 
     constructor(...props) {
-        this.openConnectionPromise = amqplib.connect(process.env.RABBITMQ_URL || process.env.AMQP_URL);
+        this.openConnection();
+    }
 
-        this.#getChannel = async () => {
-            const connection = await this.openConnectionPromise;
-            return await connection.createChannel();
+    async openConnection() {
+        try {
+
+            this.#connection = await amqplib.connect(process.env.RABBITMQ_URL || process.env.AMQP_URL);
+            this.#connection.on('error', (err) => {
+                console.error(`AMQP client connection error`, err);
+                this.#connection = null;
+                this.reconnect();
+            })
+
+            this.#connection.on('close', () => {
+                console.log(`AMQP client closed connection`, err);
+                this.#connection = null;
+                this.reconnect();
+            })
+
+            const factory = {
+                create: async () => {
+                    const channel = await this.#connection.createChannel();
+                    channel.on('close', () => console.log('AMQP client channel closed'));
+                    channel.on('error', () => console.error('AMQP client channel error', err));
+
+                    return channel;
+                },
+                destroy: async (channel) => {
+                    await channel.close();
+                }
+            }
+
+            this.#channelPool = genericPool.createPool(factory, {
+                max: process.env.MQ_CLIENT_MAX_POOL_SIZE || 10,
+                min: process.env.MQ_CLIENT_MIN_POOL_SIZE || 2
+            });
+
+            this.#getChannel = async () => {
+                return this.#channelPool.acquire();
+            }
+
+            clearTimeout(this.#reconnectTimeout);
+            console.log('AMQP client connected');
+
+        } catch (err) {
+            console.log(`Error connection to AMQP client`, err);
+            this.reconnect();
         }
     }
 
+    reconnect() {
+        clearTimeout(this.#reconnectTimeout);
+        this.#reconnectTimeout = setTimeout(() => this.openConnection(), process.env.MQ_CLIENT_RECONNECT_TIMEOUT || 1000);
+    }
+
     topic(name) {
-        return new topicAdapter(name, this.#getChannel());
+        return new topicAdapter(name, this.#getChannel, this.#channelPool);
     }
 
     createSubscription(...props) {
